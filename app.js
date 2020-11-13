65321c75649664d077476c0770fbe65d0ddf5f3b
KAWAGUCHI <jabberwocky1957@gmail.com>
2020-11-13 21:57:41 +0900

    回数選択ページへのルーティングを作成

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-const express = require("express");
+const express = require('express');
+const mysql = require('mysql');
+
 const app = express();
+app.use(express.urlencoded({extended:false}));//jsでHTMLのフォームの値を受け取る為に必要な定型文
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '*PhoLinG29',
+  database: 'databaseapp'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting:' + err.stack);
+    return;
+  }
+  console.log('success');
+});
+
 app.get('/', (req, res) => {
   res.render('top.ejs');
+});
+
+app.get('/submit', (req, res) => {
+  res.render('submit.ejs');
 });
 
 
